f252312b3391fc586d70d5c3b97ac3e29079adc7
Curt Hayman <curthayman@duck.com>
2024-09-28 16:49:38 -0400

    Error reporting updates

--- scanips.py
+++ scanips.py
@@ -16,11 +16,11 @@ while True:
         continue
 
     response = input("""\nEnter the type of scan you want to run
-                    1. SYN ACK Scan
-                    2. UDP Scan
-                    3. Comprehensive Scan
+                    1. SYN ACK Scan - Requires Root
+                    2. UDP Scan - Requires Root
+                    3. Comprehensive Scan - Requires Root
                     4. Regular Scan
-                    5. OS Detection
+                    5. OS Detection - Requires Root
                     6. Multiple IP inputs
                     7. Ping Scan
                     8. Vulnerability Scan
@@ -30,232 +30,174 @@ while True:
     if response == '9':
         break
 
-    # If user's input is 1, perform a SYN/ACK scan
-    elif response == '1':
-        print("Nmap Version: ", scanner.nmap_version())
-        scanner.scan(ip_addr, '1-1024', '-v -sS')
-
-        # Check if the IP address is up
-        if scanner[ip_addr].state() == 'up':
-            # Print scan information
-            print("\nScan Information:")
-            for info in scanner.scaninfo():
-                print(f"{info}: {scanner.scaninfo()[info]}")
-
-            # Print IP status
-            print(f"\nIP Status: {scanner[ip_addr].state()}")
-
-            # Print protocols
-            print("\nProtocols:")
-            for protocol in scanner[ip_addr].all_protocols():
-                print(protocol)
-
-            # Print open ports
-            print("\nOpen TCP Ports:")
-            for port in scanner[ip_addr]['tcp'].keys():
-                print(f"Port {port}: {scanner[ip_addr]['tcp'][port]['state']}")
-        else:
-            print(f"\nIP {ip_addr} is down.")
-
-    # If user's input is 2, perform a UDP scan
-    elif response == '2':
-        print("Nmap Version: ", scanner.nmap_version())
-        scanner.scan(ip_addr, '1-1024', '-v -sU')
-
-        # Check if the IP address is up
-        if scanner[ip_addr].state() == 'up':
-            # Print scan information
-            print("\nScan Information:")
-            for info in scanner.scaninfo():
-                print(f"{info}: {scanner.scaninfo()[info]}")
-
-            # Print IP status
-            print(f"\nIP Status: {scanner[ip_addr].state()}")
-
-            # Print protocols
-            print("\nProtocols:")
-            for protocol in scanner[ip_addr].all_protocols():
-                print(protocol)
-
-            # Print open ports
-            print("\nOpen UDP Ports:")
-            for port in scanner[ip_addr]['udp'].keys():
-                print(f"Port {port}: {scanner[ip_addr]['udp'][port]['state']}")
-        else:
-            print(f"\nIP {ip_addr} is down.")
-
-    # If user's input is 3, perform a Comprehensive scan
-    elif response == '3':
-        print("Nmap Version: ", scanner.nmap_version())
-        scanner.scan(ip_addr, '1-1024', '-v -sS -sV -sC -A -O')
-
-        # Check if the IP address is up
-        if scanner[ip_addr].state() == 'up':
-            # Print scan information
-            print("\nScan Information:")
-            for info in scanner.scaninfo():
-                print(f"{info}: {scanner.scaninfo()[info]}")
-
-            # Print IP status
-            print(f"\nIP Status: {scanner[ip_addr].state()}")
-
-            # Print protocols
-            print("\nProtocols:")
-            protocols = scanner[ip_addr].all_protocols()
-            for protocol in protocols:
-                print(protocol)
-
-            # Print open ports
-            print("\nOpen Ports:")
-            for protocol in protocols:
-                print(f"\nOpen {protocol} Ports:")
-                for port in scanner[ip_addr][protocol].keys():
-                    print(f"Port {port}: {scanner[ip_addr][protocol][port]['state']}")
-        else:
-            print(f"\nIP {ip_addr} is down.")
-
-    # If user's input is 4, perform a Regular Scan
-    elif response == '4':
-        scanner.scan(ip_addr)
-
-        # Check if the IP address is up
-        if scanner[ip_addr].state() == 'up':
-            # Print scan information
-            print("\nScan Information:")
-            for info in scanner.scaninfo():
-                print(f"{info}: {scanner.scaninfo()[info]}")
-
-            # Print IP status
-            print(f"\nIP Status: {scanner[ip_addr].state()}")
-
-            # Print protocols
-            print("\nProtocols:")
-            for protocol in scanner[ip_addr].all_protocols():
-                print(protocol)
-
-            # Print open ports
-            print("\nOpen TCP Ports:")
-            for port in scanner[ip_addr]['tcp'].keys():
-                print(f"Port {port}: {scanner[ip_addr]['tcp'][port]['state']}")
-        else:
-            print(f"\nIP {ip_addr} is down.")
-
-    elif response == '5':
-
-        scan_result = scanner.scan(ip_addr, arguments="-O")
-
-        # Check if the IP address is in the scan results
-
-        if ip_addr in scan_result['scan']:
-
+    try:
+        # If user's input is 1, perform a SYN/ACK scan
+        if response == '1':
+            print("Nmap Version: ", scanner.nmap_version())
+            scanner.scan(ip_addr, '1-1024', '-v -sS')
             # Check if the IP address is up
-
-            if scan_result['scan'][ip_addr]['status']['state'] == 'up':
-
-                # Print OS information
-
-                if 'osmatch' in scan_result['scan'][ip_addr]:
-
-                    if len(scan_result['scan'][ip_addr]['osmatch']) > 0:
-
-                        print("\nOS Match:")
-
-                        print(scan_result['scan'][ip_addr]['osmatch'][0])
-
-                    else:
-
-                        print("\nNo OS matches found.")
-
-                else:
-
-                    print("\nNo OS information available.")
-
-            else:
-
-                print(f"\nIP {ip_addr} is down.")
-
-        else:
-
-            print(f"\nNo information available for IP {ip_addr}.")
-
-    elif response == '6':
-        ip_addr = input("Please enter multiple IP addresses (space separated): ")
-        ip_addrs = ip_addr.split()
-
-        # Scan each IP address
-        for ip in ip_addrs:
-            print(f"\nScanning IP: {ip}")
-            scanner.scan(ip, '1-1024', '-v -sS')
-
-            # Check if the IP address is up
-            if scanner[ip].state() == 'up':
+            if scanner[ip_addr].state() == 'up':
                 # Print scan information
                 print("\nScan Information:")
                 for info in scanner.scaninfo():
                     print(f"{info}: {scanner.scaninfo()[info]}")
-
-                # Print IP status
-                print(f"\nIP Status: {scanner[ip].state()}")
-
-                # Print protocols
+                print(f"\nIP Status: {scanner[ip_addr].state()}")
                 print("\nProtocols:")
-                for protocol in scanner[ip].all_protocols():
+                for protocol in scanner[ip_addr].all_protocols():
                     print(protocol)
-
-                # Print open ports
                 print("\nOpen TCP Ports:")
-                for port in scanner[ip]['tcp'].keys():
-                    print(f"Port {port}: {scanner[ip]['tcp'][port]['state']}")
+                for port in scanner[ip_addr]['tcp'].keys():
+                    print(f"Port {port}: {scanner[ip_addr]['tcp'][port]['state']}")
             else:
-                print(f"\nIP {ip} is down.")
+                print(f"\nIP {ip_addr} is down.")
 
-    elif response == '7':
-        ip_addr = input("Please enter the network address (e.g. 192.168.1.0/24): ")
-        scanner.scan(hosts=ip_addr, arguments='-n -sP -PE -PA21,23,80,3389')
+        # If user's input is 2, perform a UDP scan
+        elif response == '2':
+            print("Nmap Version: ", scanner.nmap_version())
+            scanner.scan(ip_addr, '1-1024', '-v -sU')
+            # Check if the IP address is up
+            if scanner[ip_addr].state() == 'up':
+                # Print scan information
+                print("\nScan Information:")
+                for info in scanner.scaninfo():
+                    print(f"{info}: {scanner.scaninfo()[info]}")
+                print(f"\nIP Status: {scanner[ip_addr].state()}")
+                print("\nProtocols:")
+                for protocol in scanner[ip_addr].all_protocols():
+                    print(protocol)
+                print("\nOpen UDP Ports:")
+                for port in scanner[ip_addr]['udp'].keys():
+                    print(f"Port {port}: {scanner[ip_addr]['udp'][port]['state']}")
+            else:
+                print(f"\nIP {ip_addr} is down.")
 
-        # Print host list
-        print("\nHost List:")
-        hosts_list = [(x, scanner[x]['status']['state']) for x in scanner.all_hosts()]
-        for host, status in hosts_list:
-            print(f"{host}: {status}")
+        # If user's input is 3, perform a Comprehensive scan
+        elif response == '3':
+            print("Nmap Version: ", scanner.nmap_version())
+            scanner.scan(ip_addr, '1-1024', '-v -sS -sV -sC -A -O')
+            # Check if the IP address is up
+            if scanner[ip_addr].state() == 'up':
+                # Print scan information
+                print("\nScan Information:")
+                for info in scanner.scaninfo():
+                    print(f"{info}: {scanner.scaninfo()[info]}")
+                print(f"\nIP Status: {scanner[ip_addr].state()}")
+                print("\nProtocols:")
+                protocols = scanner[ip_addr].all_protocols()
+                for protocol in protocols:
+                    print(protocol)
+                print("\nOpen Ports:")
+                for protocol in protocols:
+                    print(f"\nOpen {protocol} Ports:")
+                    for port in scanner[ip_addr][protocol].keys():
+                        print(f"Port {port}: {scanner[ip_addr][protocol][port]['state']}")
+            else:
+                print(f"\nIP {ip_addr} is down.")
 
-    elif response == '8':
-        scanner.scan(ip_addr, '1-1024', '-sV --script=vuln')
+        # If user's input is 4, perform a Regular Scan
+        elif response == '4':
+            scanner.scan(ip_addr)
+            # Check if the IP address is up
+            if scanner[ip_addr].state() == 'up':
+                #```python
+                # Print scan information
+                print("\nScan Information:")
+                for info in scanner.scaninfo():
+                    print(f"{info}: {scanner.scaninfo()[info]}")
+                print(f"\nIP Status: {scanner[ip_addr].state()}")
+                print("\nProtocols:")
+                for protocol in scanner[ip_addr].all_protocols():
+                    print(protocol)
+                print("\nOpen TCP Ports:")
+                for port in scanner[ip_addr]['tcp'].keys():
+                    print(f"Port {port}: {scanner[ip_addr]['tcp'][port]['state']}")
+            else:
+                print(f"\nIP {ip_addr} is down.")
 
-        # Check if the IP address is up
-        if scanner[ip_addr].state() == 'up':
-            # Print scan information
-            print("\nScan Information:")
-            for info in scanner.scaninfo():
-                print(f"{info}: {scanner.scaninfo()[info]}")
+        elif response == '5':
+            scan_result = scanner.scan(ip_addr, arguments="-O")
+            # Check if the IP address is in the scan results
+            if ip_addr in scan_result['scan']:
+                # Check if the IP address is up
+                if scan_result['scan'][ip_addr]['status']['state'] == 'up':
+                    # Print OS information
+                    if 'osmatch' in scan_result['scan'][ip_addr]:
+                        if len(scan_result['scan'][ip_addr]['osmatch']) > 0:
+                            print("\nOS Match:")
+                            print(scan_result['scan'][ip_addr]['osmatch'][0])
+                        else:
+                            print("\nNo OS matches found.")
+                    else:
+                        print("\nNo OS information available.")
+                else:
+                    print(f"\nIP {ip_addr} is down.")
+            else:
+                print(f"\nNo information available for IP {ip_addr}.")
 
-            # Print IP status
-            print(f"\nIP Status: {scanner[ip_addr].state()}")
+        elif response == '6':
+            ip_addr = input("Please enter multiple IP addresses (space separated): ")
+            ip_addrs = ip_addr.split()
+            # Scan each IP address
+            for ip in ip_addrs:
+                print(f"\nScanning IP: {ip}")
+                scanner.scan(ip, '1-1024', '-v -sS')
+                # Check if the IP address is up
+                if scanner[ip].state() == 'up':
+                    # Print scan information
+                    print("\nScan Information:")
+                    for info in scanner.scaninfo():
+                        print(f"{info}: {scanner.scaninfo()[info]}")
+                    print(f"\nIP Status: {scanner[ip].state()}")
+                    print("\nProtocols:")
+                    for protocol in scanner[ip].all_protocols():
+                        print(protocol)
+                    print("\nOpen TCP Ports:")
+                    for port in scanner[ip]['tcp'].keys():
+                        print(f"Port {port}: {scanner[ip]['tcp'][port]['state']}")
+                else:
+                    print(f"\nIP {ip} is down.")
 
-            # Print protocols
-            print("\nProtocols:")
-            for protocol in scanner[ip_addr].all_protocols():
-                print(protocol)
+        elif response == '7':
+            ip_addr = input("Please enter the network address (e.g. 192.168.1.0/24): ")
+            scanner.scan(hosts=ip_addr, arguments='-n -sP -PE -PA21,23,80,3389')
+            # Print host list
+            print("\nHost List:")
+            hosts_list = [(x, scanner[x]['status']['state']) for x in scanner.all_hosts()]
+            for host, status in hosts_list:
+                print(f"{host}: {status}")
 
-            # Print open ports
-            print("\nOpen Ports:")
-            for protocol in scanner[ip_addr].all_protocols():
-                print(f"\nOpen {protocol} Ports:")
-                for port in scanner[ip_addr][protocol].keys():
-                    print(f"Port {port}: {scanner[ip_addr][protocol][port]['state']}")
+        elif response == '8':
+            scanner.scan(ip_addr, '1-1024', '-sV --script=vuln')
+            # Check if the IP address is up
+            if scanner[ip_addr].state() == 'up':
+                # Print scan information
+                print("\nScan Information:")
+                for info in scanner.scaninfo():
+                    print(f"{info}: {scanner.scaninfo()[info]}")
+                print(f"\nIP Status: {scanner[ip_addr].state()}")
+                print("\nProtocols:")
+                for protocol in scanner[ip_addr].all_protocols():
+                    print(protocol)
+                print("\nOpen Ports:")
+                for protocol in scanner[ip_addr].all_protocols():
+                    print(f"\nOpen {protocol} Ports:")
+                    for port in scanner[ip_addr][protocol].keys():
+                        print(f"Port {port}: {scanner[ip_addr][protocol][port]['state']}")
+                # Print vulnerability information
+                print("\nVulnerability Information:")
+                for host in scanner.all_hosts():
+                    for proto in scanner[host].all_protocols():
+                        print(f"\nProtocol: {proto}")
+                        lport = scanner[host][proto].keys()
+                        sorted(lport)
+                        for port in lport:
+                            print(f"Port: {port}")
+                            print(scanner[host][proto][port])
+            else:
+                print(f"\nIP {ip_addr} is down.")
 
-            # Print vulnerability information
-            print("\nVulnerability Information:")
-            for host in scanner.all_hosts():
-                for proto in scanner[host].all_protocols():
-                    print(f"\nProtocol: {proto}")
-                    lport = scanner[host][proto].keys()
-                    sorted(lport)
-                    for port in lport:
-                        print(f"Port: {port}")
-                        print(scanner[host][proto][port])
         else:
-            print(f"\nIP {ip_addr} is down.")
+            print("Please choose a number from the options above")
 
-    else:
-        print("Please choose a number from the options above")
+    except nmap.PortScannerError as e:
+        print(f"Error: {str(e)}. This operation requires root privileges. Please run the script with elevated permissions.")
